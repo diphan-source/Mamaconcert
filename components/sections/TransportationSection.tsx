10ff71d3c9ea5dd98635cf7895f320a73ce45c56
diphan-source <ahabwediphan@yahoo.com>
2026-01-08 00:15:14 -0500

    fix

--- components/sections/TransportationSection.tsx
+++ components/sections/TransportationSection.tsx
@@ -1,5 +1,4 @@
-// "use client";
-
+"use client";
 import React, { useRef, useState } from "react";
 import { motion, useInView } from "framer-motion";
 import { FaBus, FaDownload } from "react-icons/fa";
